22f79a9805b46d001b2e251198be9ef5e392ea9a
muhammad imran <muhammad.imran1481@gmail.com>
2020-09-10 23:49:13 +0500

    adding start scripts in package.json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-
+const port = process.env.PORT || 8080;
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist/cpeauto"));
 
@@ -11,4 +11,6 @@ app.get("/*", function (req, res) {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
